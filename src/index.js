6a96459fb662eadd4a4343055d407405d4e200f8
made <a.grandin42@gmail.com>
2018-06-05 14:01:10 +0200

    use event on components to use is data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {render} from 'react-dom'
 import Button from 'components/Button'
+import Input from 'components/Input'
 
 render(
     <div>
         <h1>Hello Anthony !</h1>
         <Button/>
+        <Input name="test"/>
     </div>,
     document.querySelector('#mount')
 )
