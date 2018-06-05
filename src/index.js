dc6ffdf00d39054481ab1421daf16fd2669bf5ea
made <a.grandin42@gmail.com>
2018-06-05 13:38:03 +0200

    add hello button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
+import Button from 'components/Button'
 
 render(
-    <h1>Hello Anthony !</h1>,
+    <div>
+        <h1>Hello Anthony !</h1>
+        <Button/>
+    </div>,
     document.querySelector('#mount')
 )
